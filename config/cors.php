9ee01c570b9c2e0d385eab47c28274fe5847d404
naufal <naufalopo87@gmail.com>
2025-11-05 19:59:23 +0700

    feat: add cors

---
+++ config/cors.php
@@ -0,0 +1,25 @@
+<?php
+return [
+
+    'paths' => [
+        'api/*',
+        'sanctum/csrf-cookie',
+    ],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => ['*'],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [
+        'Authorization', 'Content-Type', 'X-RateLimit-Limit',
+        'X-RateLimit-Remaining', 'X-RateLimit-Reset'
+    ],
+
+    'max_age' => 3600,
+
+    'supports_credentials' => false,
+];
