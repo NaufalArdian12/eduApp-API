413ada0f3d4e08ca0d90f09ffc78e1d35885c3fe
naufal <naufalopo87@gmail.com>
2025-11-22 20:56:15 +0700

    feat: add User UI admin

---
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Filament\Resources\Users\Schemas;
+
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Schema;
+
+class UserForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->required(),
+                TextInput::make('email')
+                    ->label('Email address')
+                    ->email()
+                    ->required(),
+                DateTimePicker::make('email_verified_at'),
+                TextInput::make('password')
+                    ->password()
+                    ->required(),
+                TextInput::make('avatar_url')
+                    ->url(),
+                TextInput::make('role')
+                    ->required()
+                    ->default('student'),
+                DateTimePicker::make('last_login_at'),
+                Toggle::make('is_admin')
+                    ->required(),
+            ]);
+    }
+}
