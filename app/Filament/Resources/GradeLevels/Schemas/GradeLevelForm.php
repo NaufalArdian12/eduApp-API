9f66a23705b48c4501d8ca4bbd2c8c5d25e4e3e8
naufal <naufalopo87@gmail.com>
2025-11-22 20:55:15 +0700

    feat: add grade levels UI

---
+++ app/Filament/Resources/GradeLevels/Schemas/GradeLevelForm.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Filament\Resources\GradeLevels\Schemas;
+
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Schema;
+
+class GradeLevelForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                Select::make('subject_id')
+                    ->relationship('subject', 'name')
+                    ->required(),
+                TextInput::make('grade_no')
+                    ->required()
+                    ->numeric(),
+                TextInput::make('name')
+                    ->required(),
+                Textarea::make('description')
+                    ->columnSpanFull(),
+                TextInput::make('order_index')
+                    ->numeric(),
+                Toggle::make('is_active')
+                    ->required(),
+            ]);
+    }
+}
